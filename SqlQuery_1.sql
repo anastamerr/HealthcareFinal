c63548eaae685e4d85378017f7c25697c85b942d
anastamerr <105374455+anastamerr@users.noreply.github.com>
2024-05-14 20:26:19 +0300

    Add files via upload
    SQL Queries that are required to created the DataBase tables

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,1 +1,17 @@
-﻿INSERT INTO Patient (UserEmail,Password,SSN)VALUES('anas','1313',14);
+﻿CREATE PROCEDURE EditPharrProfile
+    @identificationCode INT,
+    @name VARCHAR(50),
+    @pharmacy_name VARCHAR(50),
+    @first_working_day VARCHAR(50)
+    
+
+AS
+BEGIN
+    UPDATE Pharmacists
+    SET 
+        name = @name,
+        pharmacy_name = @pharmacy_name,
+        first_working_day = @first_working_day
+    WHERE identificationCode = @identificationCode;
+END;
+GO
---
+++ dbo.Patient.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Patient] (
+    [UserEmail]                   VARCHAR (50) NOT NULL,
+    [Password]                    VARCHAR (20) NOT NULL,
+    [SSN]                         INT          NOT NULL,
+    [ElectronicHealthRecords]     VARCHAR (50) NULL,
+    [emergencyContactInformation] VARCHAR (50) NULL,
+    [allergies]                   VARCHAR (50) NULL,
+    [chronicDiseases]             VARCHAR (50) NULL,
+    [vaccines]                    VARCHAR (50) NULL,
+    [prescribedDrugs]             VARCHAR (50) NULL,
+    [results]                     VARCHAR (50) NULL,
+    [Hospital_name]               VARCHAR (50) NULL,
+    [companyid]                   INT          NULL,
+    [nurseID]                     INT          NULL,
+    PRIMARY KEY CLUSTERED ([SSN] ASC),
+    UNIQUE NONCLUSTERED ([UserEmail] ASC),
+    FOREIGN KEY ([nurseID]) REFERENCES [dbo].[Nurse] ([Nurse_ID]),
+    FOREIGN KEY ([Hospital_name]) REFERENCES [dbo].[Hospital] ([Hospital_name])
+);
+
